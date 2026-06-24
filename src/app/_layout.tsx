a0d8db48d6abe1d35c71b7b2beb501cd5fc39fe0
Arsh <120127753+ArshdevSinghji@users.noreply.github.com>
2026-06-24 22:13:31 +0530

    Feature: Manually installed Reusables component library

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,16 @@
-import { Stack } from "expo-router";
+import { Stack, ThemeProvider } from "expo-router";
 
 import "../global.css";
+import { StatusBar } from "expo-status-bar";
+import { PortalHost } from "@rn-primitives/portal";
+import { NAV_THEME } from "@/lib/theme";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ThemeProvider value={NAV_THEME['light']}>
+      <StatusBar style="dark" />
+      <Stack />
+      <PortalHost />
+    </ThemeProvider>
+  );
 }
